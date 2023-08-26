3c7296c24c49e917c290e9a42546e7764dd46801
Elijah Mottey <elijahmottey5@gmail.com>
2023-08-26 20:05:48 +0000

    survey form:checkbox and submit message

---
+++ src/forms/form.js
@@ -0,0 +1,30 @@
+import React , { useState } from "react"
+
+
+const Forms = () =>{
+
+    const [Submit,setSubmit] =useState("click to submit");
+    const HandleSubmit = () =>{
+        setSubmit(()=>alert("forms submitted"))
+    }
+    return(
+        <>
+        <form >
+            <div className="section-1">
+              <label><input type="checkbox"/>Excellence</label>
+              <label><input type="checkbox"/>Very Good</label>
+            </div>
+            <div className="section-2">
+              <label><input type="checkbox"/>Average</label>
+              <label><input type="checkbox"/>Poor</label>
+
+            </div>
+            {Submit} <br/>
+            <button onClick={ HandleSubmit }>Submit</button>
+        </form>
+        </>
+    )
+
+}
+
+export default Forms
